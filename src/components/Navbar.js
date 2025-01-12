d5aa2a89f4defe5ceb4850ebc0a40600612e6417
Whisdom-dev <whizddaniel@gmail.com>
2025-01-12 22:32:11 +0100

    upates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,18 +6,17 @@ const Navbar = () => {
   const { cart } = useContext(CartContext);
 
   return (
-    <nav className="flex flex-wrap justify-between items-center bg-blue-500 p-4 shadow-md text-white">
-      <div className="flex  items-center">
+    <nav className="bg-blue-500 p-4 shadow-md text-white">
+      <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">E-Grocery</h1>
-        </div>
-        <div className="hidden sm:flex space-x-4">
+        <div>
           <Link to="/" className="mr-4 hover:underline">Home</Link>
           <Link to="/checkout" className="mr-4 hover:underline">Checkout</Link>
           <Link to="/cart" className="hover:underline">
             Cart: {cart.length > 0 ? `${cart.length} items` : "Empty"}
           </Link>
         </div>
-      
+      </div>
     </nav>
   );
 };
